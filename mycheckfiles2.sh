0012a9cf8bb2749692f8c368bffd460a5c88a1c5
AWS ParallelCluster user <ec2-user@ip-10-0-8-116.ap-southeast-1.compute.internal>
2026-09-03 14:23:58 +0000

    Add renamed Lab 4 scripts (my*.sh)

---
+++ mycheckfiles2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Number of hkl files"
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+
+echo "Number of mda files"
+find mountains -name "firings.mda" | wc -l
+
+echo "#==========================================================="
+echo "Start Times"
+head -1 rplpl-slurm*.out rplsplslurm*.out
+
+echo "End Times"
+tail -n -5 rplpl-slurm*.out rplsplslurm*.out
+
+echo "#==========================================================="
